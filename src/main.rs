9ab596c69f6e0d4210f12a99fc96cae413cd93ff
Lam Nguyen <lamnhh.cs@gmail.com>
2021-05-29 12:09:10 +0700

    update read_matrix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+extern crate curve25519_dalek;
+
+use curve25519_dalek::scalar::Scalar;
 #[allow(unused_imports)]
 use libspartan::{InputsAssignment, Instance, SNARKGens, VarsAssignment, SNARK};
 #[allow(unused_imports)]
@@ -6,6 +9,23 @@ use merlin::Transcript;
 use ndarray::{Array1, Array2, Axis};
 use ndarray_npy::read_npy;
 use std::cmp;
+
+fn read_matrix(path: &str) -> Vec<(usize, usize, [u8; 32])> {
+    let mut ans: Vec<(usize, usize, [u8; 32])> = Vec::new();
+    let raw: Array2<i32> = read_npy(path).unwrap();
+    let n_row = raw.shape()[0];
+    for row in 0..n_row {
+        let u = raw[(row, 0)] as usize;
+        let v = raw[(row, 1)] as usize;
+        let w = if raw[(row, 2)] < 0 {
+            (-Scalar::from((-raw[(row, 2)]) as u32)).to_bytes()
+        } else {
+            Scalar::from(raw[(row, 2)] as u32).to_bytes()
+        };
+        ans.push((u, v, w))
+    }
+    return ans;
+}
 
 fn to_bytes(n: i32) -> [u8; 32] {
     let mut a: [u8; 32] = [0; 32];
@@ -18,26 +38,12 @@ fn to_bytes(n: i32) -> [u8; 32] {
 }
 
 fn main() {
-    let raw_a: Array2<i32> = read_npy("data/a.npy").unwrap();
-    let raw_b: Array2<i32> = read_npy("data/b.npy").unwrap();
-    let raw_c: Array2<i32> = read_npy("data/c.npy").unwrap();
-
     #[allow(non_snake_case)]
-    let mut A: Vec<(usize, usize, [u8; 32])> = Vec::new();
+    let A = read_matrix("data/a.npy");
     #[allow(non_snake_case)]
-    let mut B: Vec<(usize, usize, [u8; 32])> = Vec::new();
+    let B = read_matrix("data/b.npy");
     #[allow(non_snake_case)]
-    let mut C: Vec<(usize, usize, [u8; 32])> = Vec::new();
-
-    for row in raw_a.axis_iter(Axis(0)) {
-        A.push((row[0] as usize, row[1] as usize, to_bytes(row[2])));
-    }
-    for row in raw_b.axis_iter(Axis(0)) {
-        B.push((row[0] as usize, row[1] as usize, to_bytes(row[2])));
-    }
-    for row in raw_c.axis_iter(Axis(0)) {
-        C.push((row[0] as usize, row[1] as usize, to_bytes(row[2])));
-    }
+    let C = read_matrix("data/c.npy");
 
     let num_cons = 3702739 + 1;
     let num_vars = 4105740;
@@ -49,34 +55,38 @@ fn main() {
     let gens = SNARKGens::new(num_cons, num_vars, num_inputs, num_nz);
     println!("[+] gens");
     let (comm, decomm) = SNARK::encode(&inst, &gens);
-    println!("[+] encode")
+    println!("[+] encode");
 
-    // let mut vars: Vec<[u8; 32]> = Vec::new();
-    // let mut inps: Vec<[u8; 32]> = Vec::new();
-    // let raw_w: Array1<i32> = read_npy("data/w.npy").unwrap();
-    // for (i, &val) in raw_w.iter().enumerate() {
-    //     if i == num_vars {
-    //         continue;
-    //     }
-    //     if i < num_vars {
-    //         vars.push(to_bytes(val));
-    //     } else {
-    //         inps.push(to_bytes(val));
-    //     }
-    // }
+    let mut vars: Vec<[u8; 32]> = Vec::new();
+    let mut inps: Vec<[u8; 32]> = Vec::new();
+    let raw_w: Array1<i32> = read_npy("data/w.npy").unwrap();
+    for (i, &val) in raw_w.iter().enumerate() {
+        if i == num_vars {
+            continue;
+        }
+        if i < num_vars {
+            vars.push(to_bytes(val));
+        } else {
+            inps.push(to_bytes(val));
+        }
+    }
 
-    // let assignment_vars = VarsAssignment::new(&vars).unwrap();
-    // let assignment_inps = InputsAssignment::new(&inps).unwrap();
+    println!("[+] read w");
 
-    // let mut prover_transcript = Transcript::new(b"snark_example");
-    // let proof = SNARK::prove(
-    //     &inst,
-    //     &decomm,
-    //     assignment_vars,
-    //     &assignment_inps,
-    //     &gens,
-    //     &mut prover_transcript,
-    // );
+    let assignment_vars = VarsAssignment::new(&vars).unwrap();
+    let assignment_inps = InputsAssignment::new(&inps).unwrap();
+
+    let mut prover_transcript = Transcript::new(b"snark_example");
+    println!("[+] prepare proof");
+    let proof = SNARK::prove(
+        &inst,
+        &decomm,
+        assignment_vars,
+        &assignment_inps,
+        &gens,
+        &mut prover_transcript,
+    );
+    println!("[+] calculated proof");
 
     // let mut verifier_transcript = Transcript::new(b"snark_example");
     // assert!(proof
